9ef5c222e36e92ca3845c0ae9d749c841d1ecdca
Kalpana Muduli <coursekalpana@gmail.com>
2024-06-28 20:13:11 +0530

    urls and setting vite

---
+++ src/api/index.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+const homeApiEndPoint = "https://pokeapi.co/api/v2/pokemon?limit=90";
+const baseImageUrl =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world";
+const basePokemonDetailsUrl = "https://pokeapi.co/api/v2/pokemon";
